3e8a482f01fd123866adad600be34a11c17876f2
jojoe-ainoo <jojoeainoo@gmail.com>
2023-07-11 03:06:01 -0400

    feature/custom-hash

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from core.models import Url
 from core.forms import UrlForm
+
 
 
 class HomeView(View):
@@ -16,12 +19,22 @@ class HomeView(View):
         if not form.is_valid():
             return render(request, self.template_name, {"form": form})
 
-        url = form.cleaned_data.get("url")
-        obj = Url.objects.create(url=url)
+        url = form.cleaned_data["url"]
+        hashed_url = form.cleaned_data.get("hashed_url")
+        existing_url = Url.objects.filter(hashed_url=hashed_url).first()
 
-        return render(
-            request, self.template_name, {"short_url": obj.get_full_short_url()}
-        )
+        if hashed_url and existing_url:
+            form.add_error("hashed_url", "This custom hash is already taken!")
+        else:
+            obj = Url.objects.create(url=url, hashed_url=hashed_url)
+            if obj:
+                return render(
+                    request,
+                    self.template_name,
+                    {"form": form, "short_url": obj.get_full_short_url()},
+                )
+
+        return render(request, self.template_name, {"form": form})
 
 class RedirectView(View):
     def get(self, request, *args, **kwargs):
